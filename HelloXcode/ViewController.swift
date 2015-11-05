bbb6f9b1955af1a220243e7877e892fdb3a7884a
EDUMobile Developer <edumobiledev@richards-mac-mini.local>
2015-11-05 08:27:42 -0600

    Initial Commit

---
+++ HelloXcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloXcode
+//
+//  Created by Richard Campbell on 11/5/15.
+//  Copyright © 2015 Edumobile Academy | www.edumobile.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
